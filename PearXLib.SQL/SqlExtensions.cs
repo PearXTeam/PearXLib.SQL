9cd109f6eaf063766cdd19c5ccc990a62bfd750f
mrAppleXZ <mr.applexz@gmail.com>
2017-04-24 15:19:20 +0500

    Move a lot of methods in PXL

---
+++ PearXLib.SQL/SqlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PearXLib.SQL
+{
+	/// <summary>
+	/// MySQL extensions.
+	/// </summary>
+	public static class SqlExtensions
+	{
+		/// <summary>
+		/// Adds an escaped string to the ParameterCollection for using with LIKE request.
+		/// </summary>
+		/// <param name="p">Parameter collection.</param>
+		/// <param name="name">Name.</param>
+		/// <param name="value">Value.</param>
+		public static void AddLikeEscaped(this MySqlParameterCollection p, string name, string value)
+		{
+			p.AddWithValue(name, SQLUtils.EscapePrepared(value));
+		}
+	}
+}
